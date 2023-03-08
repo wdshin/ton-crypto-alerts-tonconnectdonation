1f721253a3b61e005e754a3acce79db254045095
Alex Kim <alexkimtwink@icloud.com>
2023-03-09 01:42:08 +0500

    custom ton manifest
    Prepared for custom dApp manifest

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 // this manifest is used temporarily for development purposes
 const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+  "https://raw.githubusercontent.com/ChemicalLogic/tonconnectdonation/main/manifest/tonconnect-manifest.json";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
