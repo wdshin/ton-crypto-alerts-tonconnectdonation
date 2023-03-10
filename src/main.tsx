9fe601a6e5824e72dd609ed2ed4e6f84c9c21263
Alex Kim <alexkimtwink@icloud.com>
2023-03-10 16:13:22 +0500

    prod manifest fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 // this manifest is used temporarily for development purposes
 const manifestUrl =
-  "https://raw.githubusercontent.com/ChemicalLogic/tonconnectdonation/main/manifest/tonconnect-manifest.json";
+  "https://raw.githubusercontent.com/TonCryptoAlerts/tonconnectdonation/main/manifest/tonconnect-manifest.json";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
